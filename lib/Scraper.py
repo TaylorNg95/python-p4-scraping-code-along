e7f7f061ef3a846454ca0844959f24ab75d0b5d2
Taylor Ng <taylormng95@gmail.com>
2024-07-16 17:26:05 -0400

    completes lab

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -5,4 +5,32 @@ import ipdb
 
 
 class Scraper:
-    pass
+
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        html = requests.get('https://learn-co-curriculum.github.io/site-for-scraping/courses')
+        doc = BeautifulSoup(html.text, 'html.parser')
+        return doc
+
+    def get_courses(self):
+        courses = self.get_page().select('.post')
+        return courses
+
+    def make_courses(self):
+        courses = self.get_courses()
+        for course in courses:
+            title = course.select('h2')[0].text if course.select('h2') else ''
+            schedule = course.select('.date')[0].text if course.select('.date') else ''
+            description = course.select('p')[0].text if course.select('p') else ''
+            new_course = Course(title=title, schedule=schedule, description=description)
+            self.courses.append(new_course)
+        return self.courses
+
+    def print_courses(self):
+        courses = self.make_courses()
+        for course in courses:
+            print(course)
+
+Scraper().print_courses()
